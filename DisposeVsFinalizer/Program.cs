d1d4c0cd8cbf534fbf0aa41b9ef591607a868c76
Rodion Mostovoi <rodion.mostovoy@outlook.com>
2024-01-07 14:56:57 +0800

    Add memory profiling to DisposeVsFinalizer project
    The JetBrains.Profiler.Api package has been added to the DisposeVsFinalizer.csproj file. Corresponding changes have been made in the Program.cs file, enabling memory profiling for the application. This includes taking memory snapshots at different stages of execution and monitoring GC collections. The changes aim to provide deeper insights into the application's memory usage and garbage collection behavior.

--- DisposeVsFinalizer/Program.cs
+++ DisposeVsFinalizer/Program.cs
@@ -1,27 +1,61 @@
 ﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using JetBrains.Profiler.Api;
 using ObjectLayoutInspector;
 
+bool enableProfiler = true;
+if (enableProfiler)
+{
+    /*
+     * To enable the API, you must start a profiling session with the enabled API:
+     * In Rider goto:
+     *  Run - Switch Profiling Configuration - Edit Configurations - Control Profiling via API
+     * https://www.jetbrains.com/help/dotmemory/Profiling_Guidelines__Advanced_Profiling_Using_dotTrace_API.html#profiling-a-specific-part-of-the
+     */
+    MemoryProfiler.CollectAllocations(true);
+    MemoryProfiler.GetSnapshot("Before running the timer");
+}
+
 Methods.RunTimer();
+if (enableProfiler)
+{
+    MemoryProfiler.GetSnapshot("After running the timer");
+}
 Console.WriteLine("Timer has started");
 Console.WriteLine("Press enter to run GC.Collect()");
 Console.ReadLine();
-GC.Collect(0); // После запуска GC таймер диспозится, т.к. больше нет ссылок на него. Хотя, метод диспоуз явно не вызывался.
+GC.Collect(); // После запуска GC таймер диспозится, т.к. больше нет ссылок на него. Хотя, метод диспоуз явно не вызывался.
+if (enableProfiler)
+{
+    MemoryProfiler.GetSnapshot("After the first call of GC.Collect()");
+}
 Console.WriteLine("GC.Collect() has been called");
+Console.WriteLine("Press enter to run GC.Collect() again");
+GC.Collect();
+if (enableProfiler)
+{
+    MemoryProfiler.GetSnapshot("After the second call of GC.Collect()");
+}
 Console.Read();
 
 public static class Methods
 {
-    public static IntPtr TimerPointer;
-    
     [MethodImpl(MethodImplOptions.NoInlining)]
     public static unsafe void RunTimer()
     {
+        Console.WriteLine(GC.GetGCMemoryInfo().MemoryLoadBytes);
         var timer = new Timer(_ 
             => Console.WriteLine(DateTime.Now), null, 0, 1000);
+        Console.WriteLine(GC.GetGCMemoryInfo().MemoryLoadBytes);
         
         TypeLayout.PrintLayout(timer.GetType());
-        Console.WriteLine("Generation of the timer a: " + GC.GetGeneration(timer));
+        // Console.WriteLine("Generation of the timer a: " + GC.GetGeneration(timer));
+        // GC.Collect(0);
+        // Console.WriteLine(GC.GetGCMemoryInfo().MemoryLoadBytes);
+        // Console.WriteLine("Generation of the timer b: " + GC.GetGeneration(timer));
+        // GC.Collect(1);
+        // Console.WriteLine("Generation of the timer c: " + GC.GetGeneration(timer));
+        // GC.Collect(2);
+        // Console.WriteLine("Generation of the timer d: " + GC.GetGeneration(timer));
     }
     
 }
